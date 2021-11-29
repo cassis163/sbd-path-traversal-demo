b85c250369c4b516131c06a73059fef5ae44cc22
cassis163 <cas.aangeenbrug@gmail.com>
2021-11-29 20:23:50 +0100

    Add handling for path traversal

--- app/index.php
+++ app/index.php
@@ -1,5 +1,13 @@
 <?php
-if (isset($_GET['file'])) {
-    include($_GET['file']);
+$basepath = '';
+$realBase = realpath($basepath);
+
+$userpath = $basepath . $_GET['file'];
+$realUserPath = realpath($userpath);
+
+if ($realUserPath === false || strpos($realUserPath, $realBase) !== 0) {
+    echo('Path Traversal detected!');
+} else {
+    include($userpath);
 }
 ?>
