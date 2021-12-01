f69149c4efc5f67d95182ac647fd481b69172322
cassis163 <cas.aangeenbrug@gmail.com>
2021-12-01 20:37:07 +0100

    Add explanation of fix

--- app/index.php
+++ app/index.php
@@ -1,4 +1,8 @@
 <?php
+// Basically, realpath() will resolve the provided path to an actual hard physical path (resolving symlinks, .., ., /, //, etc)...
+// So if the real user path does not start with the real base path, it is trying to do a traversal.
+// Note that the output of realpath will not have any "virtual directories" such as . or .....
+
 $basepath = '';
 $realBase = realpath($basepath);
 
